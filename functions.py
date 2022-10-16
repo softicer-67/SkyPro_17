d6a402ced969bd9f0e9bf189e8fe0b3c1e5e6260
Yuri <57116431+softicer-67@users.noreply.github.com>
2022-10-16 19:26:55 +0500

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,179 +1,86 @@
-from settings import DB_PASS
-import psycopg2
-from psycopg2 import Error
+
+def get_ads(cursor):
+    postgreSQL = "select * from new"
+
+    cursor.execute(postgreSQL)
+    shop = cursor.fetchall()
+
+    print("Вывод каждой строки и ее столбцов")
+    for row in shop:
+        print("-" * 100)
+        print("Id: ", row[0], )
+        print("Товар: ", row[1])
+        print("Описание: ", row[4])
+        print("Цена: ", row[3], '\n')
 
 
-def get_ads():
-    try:
-        # Подключиться к существующей базе данных
-        connection = psycopg2.connect(user="postgres",
-                                      # пароль, который указали при установке PostgreSQL
-                                      password=DB_PASS,
-                                      host="127.0.0.1",
-                                      port="5432",
-                                      database="new_db")
+def get_user_ads(cursor):
+    user_input = input("Введи имя: ")
+    postgreSQL = f"SELECT * FROM new JOIN author on author.c1 = new.c3 where author.c2 = '{user_input}'"
 
-        cursor = connection.cursor()
-        postgreSQL = "select * from new"
+    cursor.execute(postgreSQL)
+    shop = cursor.fetchall()
 
-        cursor.execute(postgreSQL)
-        shop = cursor.fetchall()
+    print("Вывод каждой строки и ее столбцов")
+    for row in shop:
+        # if row[8] == user_input:
+        print("-" * 100)
+        print("Id: ", row[0], )
+        print("Товар: ", row[1])
+        print("Автор: ", row[8])
+        print("Цена: ", row[3], '\n')
 
-        print("Вывод каждой строки и ее столбцов")
-        for row in shop:
+
+def get_sort(cursor):
+    print("Введи интервал цен")
+    min_input = int(input("От: "))
+    max_input = int(input("До: "))
+
+    postgreSQL = "select * from new order by c4"
+
+    cursor.execute(postgreSQL)
+    shop = cursor.fetchall()
+
+    print("Вывод каждой строки и ее столбцов")
+    for row in shop:
+        if min_input <= row[3] <= max_input:
             print("-" * 100)
-            print("Id: ", row[0], )
+            print("Id: ", row[0])
             print("Товар: ", row[1])
             print("Описание: ", row[4])
             print("Цена: ", row[3], '\n')
 
-    except (Exception, Error) as error:
-        print("Ошибка при работе с PostgreSQL", error)
-    finally:
-        if connection:
-            cursor.close()
-            connection.close()
-            print("Соединение с PostgreSQL закрыто")
 
-
-def get_user_ads():
-    user_input = input("Введи имя: ")
-    try:
-        # Подключиться к существующей базе данных
-        connection = psycopg2.connect(user="postgres",
-                                      # пароль, который указали при установке PostgreSQL
-                                      password=DB_PASS,
-                                      host="127.0.0.1",
-                                      port="5432",
-                                      database="new_db")
-
-        cursor = connection.cursor()
-        postgreSQL = f"SELECT * FROM new JOIN author on author.c1 = new.c3 where author.c2 = '{user_input}'"
-                             
-
-
-        cursor.execute(postgreSQL)
-        shop = cursor.fetchall()
-
-        print("Вывод каждой строки и ее столбцов")
-        for row in shop:
-            # if row[8] == user_input:
-            print("-" * 100)
-            print("Id: ", row[0], )
-            print("Товар: ", row[1])
-            print("Автор: ", row[8])
-            print("Цена: ", row[3], '\n')
-
-    except (Exception, Error) as error:
-        print("Ошибка при работе с PostgreSQL", error)
-    finally:
-        if connection:
-            cursor.close()
-            connection.close()
-            print("Соединение с PostgreSQL закрыто")
-
-
-def get_sort():
-    print("Введи интервал цен")
-    min_input = int(input("От: "))
-    max_input = int(input("До: "))
-    try:
-        # Подключиться к существующей базе данных
-        connection = psycopg2.connect(user="postgres",
-                                      # пароль, который указали при установке PostgreSQL
-                                      password=DB_PASS,
-                                      host="127.0.0.1",
-                                      port="5432",
-                                      database="new_db")
-
-        cursor = connection.cursor()
-        postgreSQL = "select * from new order by c4"
-
-        cursor.execute(postgreSQL)
-        shop = cursor.fetchall()
-
-        print("Вывод каждой строки и ее столбцов")
-        for row in shop:
-            if min_input <= row[3] <= max_input:
-                print("-" * 100)
-                print("Id: ", row[0])
-                print("Товар: ", row[1])
-                print("Описание: ", row[4])
-                print("Цена: ", row[3], '\n')
-
-    except (Exception, Error) as error:
-        print("Ошибка при работе с PostgreSQL", error)
-    finally:
-        if connection:
-            cursor.close()
-            connection.close()
-            print("Соединение с PostgreSQL закрыто")
-
-
-def get_city():
+def get_city(cursor):
     user_input = input("Введи город: ")
-    try:
-        # Подключиться к существующей базе данных
-        connection = psycopg2.connect(user="postgres",
-                                      # пароль, который указали при установке PostgreSQL
-                                      password=DB_PASS,
-                                      host="127.0.0.1",
-                                      port="5432",
-                                      database="new_db")
 
-        cursor = connection.cursor()
-        postgreSQL = f"SELECT * FROM new JOIN address on address.c1 = new.c3 where address.c2 like '{user_input}%'"
+    postgreSQL = f"SELECT * FROM new JOIN address on address.c1 = new.c3 where address.c2 like '{user_input}%'"
 
-        cursor.execute(postgreSQL)
-        shop = cursor.fetchall()
+    cursor.execute(postgreSQL)
+    shop = cursor.fetchall()
 
-        print("Вывод каждой строки и ее столбцов")
-        for row in shop:
-            # if row[8] == user_input:
-            print("-" * 100)
-            print("Id: ", row[0], )
-            print("Товар: ", row[1])
-            print("Автор: ", row[8])
-            print("Цена: ", row[3], '\n')
-
-    except (Exception, Error) as error:
-        print("Ошибка при работе с PostgreSQL", error)
-    finally:
-        if connection:
-            cursor.close()
-            connection.close()
-            print("Соединение с PostgreSQL закрыто")
+    print("Вывод каждой строки и ее столбцов")
+    for row in shop:
+        # if row[8] == user_input:
+        print("-" * 100)
+        print("Id: ", row[0], )
+        print("Товар: ", row[1])
+        print("Автор: ", row[8])
+        print("Цена: ", row[3], '\n')
 
 
-def get_user_price():
+def get_user_price(cursor):
     user_input = input("Введи имя: ")
-    try:
-        # Подключиться к существующей базе данных
-        connection = psycopg2.connect(user="postgres",
-                                      # пароль, который указали при установке PostgreSQL
-                                      password=DB_PASS,
-                                      host="127.0.0.1",
-                                      port="5432",
-                                      database="new_db")
 
-        cursor = connection.cursor()
-        postgreSQL = f"SELECT * FROM new JOIN author on author.c1 = new.c3 where author.c2 = '{user_input}'"
+    postgreSQL = f"SELECT * FROM new JOIN author on author.c1 = new.c3 where author.c2 = '{user_input}'"
 
-        cursor.execute(postgreSQL)
-        shop = cursor.fetchall()
+    cursor.execute(postgreSQL)
+    shop = cursor.fetchall()
 
-        print("Вывод каждой строки и ее столбцов")
-        for row in shop:
-            print("-" * 100)
-            print("Id: ", row[0], )
-            print("Товар: ", row[1])
-            print("Автор: ", row[8])
-            print("Цена: ", row[3], '\n')
-
-    except (Exception, Error) as error:
-        print("Ошибка при работе с PostgreSQL", error)
-    finally:
-        if connection:
-            cursor.close()
-            connection.close()
-            print("Соединение с PostgreSQL закрыто")
+    print("Вывод каждой строки и ее столбцов")
+    for row in shop:
+        print("-" * 100)
+        print("Id: ", row[0], )
+        print("Товар: ", row[1])
+        print("Автор: ", row[8])
+        print("Цена: ", row[3], '\n')
--- main.py
+++ main.py
@@ -1,28 +1,50 @@
+from settings import DB_PASS
+import psycopg2
+from psycopg2 import Error
 from functions import *
 
 
 def main():
-    while True:
-        print('[1] Вывести все объявления\n'
-              '[2] Вывести объявления конкретного пользователя\n'
-              '[3] Вывести объявления в диапазоне цен, сортировка данных в порядке возрастания цены\n'
-              '[4] Вывести объявления для конкретного города\n'
-              '[5] Вывести информацию для определенного пользователя и цены\n'
-              '[0] - Выход\n')
-        user_input = input('Выбрать вариант: ')
-        match user_input:
-            case '1':
-                get_ads()
-            case '2':
-                get_user_ads()
-            case '3':
-                get_sort()
-            case '4':
-                get_city()
-            case '5':
-                get_user_price()
-            case _:
-                exit('Exit')
+    try:
+        # Подключиться к существующей базе данных
+        connection = psycopg2.connect(user="postgres",
+                                      # пароль, который указали при установке PostgreSQL
+                                      password=DB_PASS,
+                                      host="127.0.0.1",
+                                      port="5432",
+                                      database="new_db")
+        cursor = connection.cursor()
+
+        while True:
+            print('[1] Вывести все объявления\n'
+                  '[2] Вывести объявления конкретного пользователя\n'
+                  '[3] Вывести объявления в диапазоне цен, сортировка данных в порядке возрастания цены\n'
+                  '[4] Вывести объявления для конкретного города\n'
+                  '[5] Вывести информацию для определенного пользователя и цены\n'
+                  '[0] - Выход\n')
+
+            user_input = input('Выбрать вариант: ')
+            match user_input:
+                case '1':
+                    get_ads(cursor)
+                case '2':
+                    get_user_ads(cursor)
+                case '3':
+                    get_sort(cursor)
+                case '4':
+                    get_city(cursor)
+                case '5':
+                    get_user_price(cursor)
+                case _:
+                    exit('Exit')
+
+    except (Exception, Error) as error:
+        print("Ошибка при работе с PostgreSQL", error)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            print("Соединение с PostgreSQL закрыто")
 
 
 if __name__ == '__main__':
